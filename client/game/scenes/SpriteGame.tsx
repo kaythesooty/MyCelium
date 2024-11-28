b7404d0660abcd7e3bd6b27b99aed975e17eff35
faelyn-jpg <169906179+faelyn-jpg@users.noreply.github.com>
2024-11-28 17:59:21 +1300

    fix movement

--- client/game/scenes/SpriteGame.tsx
+++ client/game/scenes/SpriteGame.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Phaser, { Physics, GameObjects, Scene } from 'phaser'
+import Phaser, { Physics, GameObjects, Types, Scene } from 'phaser'
 import { EventBus } from '../EventBus'
 
 export class SpriteGame extends Scene {
   background!: GameObjects.Image
   platforms!: Physics.Arcade.StaticGroup
-  player!: GameObjects.Sprite
-  stars!: GameObjects.Sprite
+  player!: Physics.Arcade.Sprite
+  stars!: GameObjects.Group
+  bombs!: GameObjects.Group
   score!: GameObjects.Text
+  scoreText!: GameObjects.Text
+  gameOver!: any
+  // cursors!: Input.Keyboard.Key
+  cursors!: Types.Input.Keyboard.CursorKeys
 
   // physics!: Physics.Arcade.ArcadePhysics
   // cursors!: Scene
@@ -24,6 +28,8 @@ export class SpriteGame extends Scene {
   create() {
     this.background = this.add.image(512, 384, 'sky')
 
+    this.player = this.physics.add.sprite(100, 450, 'dude')
+
     this.platforms = this.physics.add.staticGroup()
 
     this.platforms.create(400, 568, 'ground').setScale(2).refreshBody()
@@ -32,76 +38,84 @@ export class SpriteGame extends Scene {
     this.platforms.create(50, 250, 'ground')
     this.platforms.create(750, 220, 'ground')
 
-    this.player = this.physics.add.sprite(100, 450, 'dude')
-
     this.player.setBounce(0.2)
     this.player.setCollideWorldBounds(true)
     this.physics.add.collider(this.player, this.platforms)
 
-    // this.anims.create({
-    //   // left sprite animation (uses frames 0-3) runs at 10 frames per second
-    //   key: 'left',
-    //   frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    //   frameRate: 10,
-    //   repeat: -1, // tells animation to loop
-    // })
+    this.anims.create({
+      // left sprite animation (uses frames 0-3) runs at 10 frames per second
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1, // tells animation to loop
+    })
 
-    // this.anims.create({
-    //   // frame 4 for turning
-    //   key: 'turn',
-    //   frames: [{ key: 'dude', frame: 4 }],
-    //   frameRate: 20,
-    // })
+    this.anims.create({
+      // frame 4 for turning
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20,
+    })
 
-    // this.anims.create({
-    //   // same as left turn
-    //   key: 'right',
-    //   frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    //   frameRate: 10,
-    //   repeat: -1,
-    // })
+    this.anims.create({
+      // same as left turn
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    })
 
-    // stars = this.physics.add.group({
-    //   key: 'star',
-    //   repeat: 11,
-    //   setXY: { x: 12, y: 0, stepX: 70 }, // stepX means all children will be placed evently accross the screen
-    // })
+    this.stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+      // stepX means all children will be placed evently accross the screen
+    })
 
-    // stars.children.iterate(function (child) {
-    //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)) // randomly bounce
-    // })
-    // bombs = this.physics.add.group()
+    this.stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)) // randomly bounce
+    })
 
-    // cursors = this.input.keyboard.createCursorKeys()
+    this.bombs = this.physics.add.group()
 
-    // this.physics.add.collider(player, platforms)
-    // this.physics.add.collider(stars, platforms)
-    // this.physics.add.collider(bombs, platforms)
+    if (this.input.keyboard?.createCursorKeys()) {
+      this.cursors = this.input.keyboard?.createCursorKeys()
+    }
 
-    // this.physics.add.collider(player, bombs, hitBomb, null, this)
-    // this.physics.add.overlap(player, stars, collectStar, null, this)
-    // scoreText = this.add.text(16, 16, 'score: 0', {
-    //   fontSize: '32px',
-    //   fill: '#000',
-    // })
+    this.physics.add.collider(this.player, this.platforms)
+    this.physics.add.collider(this.stars, this.platforms)
+    this.physics.add.collider(this.bombs, this.platforms)
+
+    this.physics.add.collider(this.player, this.bombs, undefined) //undefined instead of null??? or something else? the problem is hitBomb i bet
+    // this.physics.add.overlap(
+    //   this.player,
+    //   this.stars,
+    //   this.collectStar,
+    //   null,
+    //   this,
+    // )
+    this.scoreText = this.add.text(16, 16, 'score: 0', {
+      fontSize: '32px',
+      font: '#000', //was fill before
+    })
 
     EventBus.emit('current-scene-ready', this)
   }
 
   update() {
-    // if (cursors.left.isDown) {
-    //   player.setVelocityX(-160)
-    //   player.anims.play('left', true)
-    // } else if (cursors.right.isDown) {
-    //   player.setVelocityX(160)
-    //   player.anims.play('right', true)
-    // } else {
-    //   player.setVelocityX(0)
-    //   player.anims.play('turn')
-    // }
-    // if (cursors.up.isDown && player.body.touching.down) {
-    //   player.setVelocityY(-330)
-    // }
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160)
+      this.player.anims.play('left')
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160)
+      this.player.anims.play('right', true)
+    } else {
+      this.player.setVelocityX(0)
+      this.player.anims.play('turn')
+    }
+    if (this.cursors.up.isDown && this.player.body?.touching.down) {
+      this.player.setVelocityY(-330)
+    }
   }
 
   // collectStar(player, star) {
@@ -128,14 +142,13 @@ export class SpriteGame extends Scene {
   // }
 
   // hitBomb(player, bomb) {
+  //   //instead of this. maybe scene. ?
   //   this.physics.pause()
-
-  //   player.setTint(0xff0000)
-
-  //   player.anims.play('turn')
-
-  //   gameOver = true
+  //   this.player.setTint(0xff0000)
+  //   this.player.anims.play('turn')
+  //   this.gameOver = true
   // }
+
   changeScene() {
     this.scene.start('MainMenu')
   }
