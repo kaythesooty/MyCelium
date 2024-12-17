dfa802195260e59b33ae3fd7e7b756ee9800807d
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-12-17 23:27:32 +1300

    project structure update (#80)
    * change game tiles size to better fix camera on bigger screen resolution
    
    * add file with initial constant values

---
+++ client/game/constants.ts
@@ -0,0 +1,11 @@
+export default {
+  isDebug: false, // You know what to do >:-)
+  tScale: 1, // Controls the time scale of the game for debugging purposes
+
+  tileSize: 150,
+  worldWidth: 11,
+  worldHeight: 7,
+
+  growSpeed: 90, // how long in seconds it takes a mushroom to grow one step
+  drySpeed: 120, // how long in seconds it takes a dirt tile to dry one step
+}
