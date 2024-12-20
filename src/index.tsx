508d177cecc535e70b9f1637c3cfa819e1582be3
Aleksei Stukalov <stukalov.nz@gmail.com>
2024-12-21 01:02:58 +1300

    remove auth and router code

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,8 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-// import { Auth0Provider } from '@auth0/auth0-react'
-import routes from './routes.tsx'
-
-export const router = createBrowserRouter(routes, {
-  future: {
-    v7_relativeSplatPath: true,
-    v7_fetcherPersist: true,
-    v7_partialHydration: true,
-    v7_skipActionErrorRevalidation: true,
-    v7_normalizeFormMethod: true,
-  },
-})
+import App from './App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    // <Auth0Provider
-    //   domain=""
-    //   clientId=""
-    //   authorizationParams={{
-    //     redirect_uri: window.location.origin,
-    //     audience: '',
-    //   }}
-    // >
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />,
-    // </Auth0Provider>,
+    <App />,
   )
 })
